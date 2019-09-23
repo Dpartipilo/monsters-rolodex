45c02786c0b20083ce9c28f1b33abd26c0bf160c
Diego Partipilo <Diego.Partipilo@weaveability.com>
2019-09-23 08:56:29 +0100

    Updates

---
+++ precache-manifest.cbf24cb3dc5f22a31d143c558c9a14b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ce8e0561863bc7bce8a0d1fb0e2daf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6b81e9810d16a1878b73",
+    "url": "/monsters-rolodex/static/css/main.05453553.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "6b81e9810d16a1878b73",
+    "url": "/monsters-rolodex/static/js/main.543c1794.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CompleteReactDev/monsters-rolodex/precache-manifest.d8ee5979b74129d231534844692a3908.js"
+  "/monsters-rolodex/precache-manifest.cbf24cb3dc5f22a31d143c558c9a14b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CompleteReactDev/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
