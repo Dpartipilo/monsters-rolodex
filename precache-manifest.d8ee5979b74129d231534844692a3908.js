c3a4ba95d86fa452c579723eebd8a833145184ea
Diego Partipilo <Diego.Partipilo@weaveability.com>
2019-09-23 08:53:53 +0100

    Updates

---
+++ precache-manifest.d8ee5979b74129d231534844692a3908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948f384c3c7fcc21e48dad595278bf3d",
+    "url": "/CompleteReactDev/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d35242a9ea12bd576f63",
+    "url": "/CompleteReactDev/monsters-rolodex/static/css/main.05453553.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/CompleteReactDev/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "d35242a9ea12bd576f63",
+    "url": "/CompleteReactDev/monsters-rolodex/static/js/main.cf15ed10.chunk.js"
+  },
+  {
+    "revision": "8cf49411520faec58725",
+    "url": "/CompleteReactDev/monsters-rolodex/static/js/runtime-main.ff72a981.js"
+  }
+]);
